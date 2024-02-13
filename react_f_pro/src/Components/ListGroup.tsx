a075b00b2c02f3ea676571d1a09f4c46d87afc5c
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-02-14 00:13:40 +0530

    learning about some states

--- react_f_pro/src/Components/ListGroup.tsx
+++ react_f_pro/src/Components/ListGroup.tsx
@@ -1,20 +1,29 @@
-import { MouseEventHandler } from "react";
+import { useState } from "react";
 
-let items = ["nda", "pnb", "nzm", "tpj", "hyd"];
+
 
 // items.map((item) => <li>{item}</li>);
 
-function handle_onclick(event: MouseEventHandler) {
-  console.log(event);
-}
 
 function ListGroup() {
+
+  let items = ["nda", "pnb", "nzm", "tpj", "hyd"];
+  let [selectedindex, setselectedindex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handle_onclick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedindex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setselectedindex(index)}
+          >
             {item}
           </li>
         ))}
