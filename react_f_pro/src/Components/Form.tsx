aef2353c6197780a753d681b1f118abff8d9f28e
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-02-21 17:45:45 +0530

    Starting with react hook forms

--- react_f_pro/src/Components/Form.tsx
+++ react_f_pro/src/Components/Form.tsx
@@ -1,38 +1,52 @@
 import React from 'react'
 
 import { FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Form = () => {
 
-  const [person, setPerson]=useState({
-	name: '',
-	age: 0,
-  })
+	const {register, handleSubmit}=useForm()
 
-  const handleSubmit = (event: FormEvent) =>{
-	event.preventDefault();
-	console.log(person)
-  }
+// Use of below functions and useState for name, age is now
+// not needed as we are using useForms.
 
-  function handle_namein(event: React.ChangeEvent<HTMLInputElement>){
-	setPerson({...person,name: event.target.value})
-	console.log(event)
-  }
+//   const [person, setPerson]=useState({
+// 	name: '',
+// 	age: 0,
+//   })
 
-  function handle_agein(event: React.ChangeEvent<HTMLInputElement>){
-	setPerson({...person,age: parseInt(event.target.value)})
-  }
+//   const handleSubmit = (event: FormEvent) =>{
+// 	event.preventDefault();
+// 	console.log(person)
+//   }
+
+//   function handle_namein(event: React.ChangeEvent<HTMLInputElement>){
+// 	setPerson({...person,name: event.target.value})
+// 	console.log(event)
+//   }
+
+//   function handle_agein(event: React.ChangeEvent<HTMLInputElement>){
+// 	setPerson({...person,age: parseInt(event.target.value)})
+//   }
 
   return (
 	<>
-	 <form onSubmit={handleSubmit}>
+	 <form onSubmit={handleSubmit(data => console.log(data))}>
 		<div className="mb-3">
 			<label htmlFor="name" className="form-label">Name</label>
-			<input onChange={(event) => handle_namein(event)} value={person.name} id="name" type="text" className="form-control" />
+			<input 
+			{...register('name')}
+			id="name" 
+			type="text" 
+			className="form-control" />
 		</div>
 		<div className="mb-3">
 			<label htmlFor="age" className="form-label">Age</label>
-			<input onChange={(event) => handle_agein(event)} value={person.age} id="age" type="number" className="form-control" />
+			<input 
+			{...register('age')} 
+			id="age" 
+			type="number" 
+			className="form-control" />
 		</div>
 		<button className="btn btn-primary" type="submit">Submit</button>
 	 </form>
