e52e019a8307127a8280f50a3f2667ee3fb14b12
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-02-21 15:41:50 +0530

    created a complex form, with handle_submit, object , and several use states with more focus on event (to learn more about it)

--- react_f_pro/src/Components/Form.tsx
+++ react_f_pro/src/Components/Form.tsx
@@ -1,16 +1,38 @@
 import React from 'react'
 
+import { FormEvent, useState } from 'react';
+
 const Form = () => {
+
+  const [person, setPerson]=useState({
+	name: '',
+	age: 0,
+  })
+
+  const handleSubmit = (event: FormEvent) =>{
+	event.preventDefault();
+	console.log(person)
+  }
+
+  function handle_namein(event: React.ChangeEvent<HTMLInputElement>){
+	setPerson({...person,name: event.target.value})
+	console.log(event)
+  }
+
+  function handle_agein(event: React.ChangeEvent<HTMLInputElement>){
+	setPerson({...person,age: parseInt(event.target.value)})
+  }
+
   return (
 	<>
-	 <form>
+	 <form onSubmit={handleSubmit}>
 		<div className="mb-3">
 			<label htmlFor="name" className="form-label">Name</label>
-			<input id="name" type="text" className="form-control" />
+			<input onChange={(event) => handle_namein(event)} value={person.name} id="name" type="text" className="form-control" />
 		</div>
 		<div className="mb-3">
 			<label htmlFor="age" className="form-label">Age</label>
-			<input id="age" type="number" className="form-control" />
+			<input onChange={(event) => handle_agein(event)} value={person.age} id="age" type="number" className="form-control" />
 		</div>
 		<button className="btn btn-primary" type="submit">Submit</button>
 	 </form>
