1c363876c9404a7bc85ce5d210a1e4e02c4cfded
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-02-23 15:15:32 +0530

    Handling Errors during fetching from server

--- react_f_pro/src/App.tsx
+++ react_f_pro/src/App.tsx
@@ -11,12 +11,17 @@ const App = () => {
 
   const [users, setUsers]=useState<User[]>([])
 
+  const [errorMessage, setErrorMessage] = useState("")
+
   useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users').then(response => setUsers(response.data))
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    .then(response => setUsers(response.data))// then function informs what to do after fetching
+    .catch(er_obj => setErrorMessage(er_obj.message))// catch function is to tell what to do if some eroor occurs while fetching
   }, [])
 
   return (
     <>
+    {errorMessage && <p className="text-danger">{errorMessage}</p>}
     <ul>
       {users.map(info_u => <li key={info_u.id}>Id: {info_u.id} Name: {info_u.name} Username: {info_u.username}</li>)}
     </ul>   
