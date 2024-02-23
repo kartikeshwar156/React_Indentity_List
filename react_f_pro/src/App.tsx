a6e74340b6995da0d6014b46708560ebc680c02a
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-02-23 16:24:12 +0530

    How to cancel the fetch request

--- react_f_pro/src/App.tsx
+++ react_f_pro/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import axios, { CanceledError } from 'axios';
 
 interface User{
   id: number;
@@ -13,15 +13,54 @@ const App = () => {
 
   const [errorMessage, setErrorMessage] = useState("")
 
+  
+
+  function connect(){
+		console.log("Connecting")
+	}
+
+	function Disconnect(){
+		console.log("Disconnecting")
+	}
+
   useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-    .then(response => setUsers(response.data))// then function informs what to do after fetching
-    .catch(er_obj => setErrorMessage(er_obj.message))// catch function is to tell what to do if some eroor occurs while fetching
+
+    connect();
+    // AbortController should always be called in useEffect()
+    const controller = new AbortController();
+
+    axios
+    .get<User[]>('https://jsonplaceholder.typicode.com/users', {signal: controller.signal})
+    .then(response => {
+      setUsers(response.data); 
+      console.log(response);
+    })// then function informs what to do after fetching
+    .catch(er_obj => {
+
+      // below if function is written only because of strict mode, it won't be needed in development mode.
+      if(er_obj instanceof CanceledError)
+      {
+        return;
+      }
+
+      setErrorMessage(er_obj.message)
+    })// catch function is to tell what to do if some eroor occurs while fetching
+
+    console.log("fetched")
+
+
+    return () => {
+      Disconnect();
+      controller.abort();
+    };
   }, [])
 
   return (
     <>
-    {errorMessage && <p className="text-danger">{errorMessage}</p>}
+    {errorMessage && <p className="text-danger">{errorMessage} !!!</p>}
+
+    <p>List is Below:</p>
+
     <ul>
       {users.map(info_u => <li key={info_u.id}>Id: {info_u.id} Name: {info_u.name} Username: {info_u.username}</li>)}
     </ul>   
